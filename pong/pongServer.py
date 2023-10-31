7069640697f6202b1c47428744920ef4c4809581
Keaton Martin <keatonsphone1@gmail.com>
2023-10-31 12:03:25 -0400

    add multithreaded server functionality, send init info

--- pong/pongServer.py
+++ pong/pongServer.py
@@ -1,53 +1,48 @@
 # =================================================================================================
 # Contributing Authors:	    Keaton Martin
 # Email Addresses:          ktma234@uky.edu
-# Date:                     10/27/23
+# Date:                     10/29/23
 # Purpose:                  The pong server implementation.
-# Misc:                     <Not Required.  Anything else you might want to include>
 # =================================================================================================
 
 import socket
 import threading
 
-# Use this file to write your server logic
-# You will need to support at least two clients
-# You will need to keep track of where on the screen (x,y coordinates) each paddle is, the score 
-# for each player and where the ball is, and relay that to each client
-# I suggest you use the sync variable in pongClient.py to determine how out of sync your two
-# clients are and take actions to resync the games
-
 HOST = "localhost"
 PORT = 12321
 MAX_CONNS = 10 # should probably be even 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
 
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server.bind((HOST, PORT))
-
-server.listen(MAX_CONNS)
-
-def handleGame(c1, c2):
-    print("Two clients are connected to me")
-
-while True: 
-    # attempt to pair two incoming clients 
-    clientSocket1, _ = server.accept()
-    print("Player one has connected.")
-    clientSocket2, _ = server.accept()
-    print("Player two has connected.")
+def handleGame(c1: socket.socket, c2: socket.socket) -> None:
+    # send paddle identities to clients
+    c1.send(f"left {SCREEN_WIDTH} {SCREEN_HEIGHT}".encode()) 
+    c2.send(f"right {SCREEN_WIDTH} {SCREEN_HEIGHT}".encode())
     
-    gameThread = threading.Thread(target=handleGame, args=(clientSocket1, clientSocket2))
-    gameThread.run()
+    # start game
+    while True:
+        c1state = c1.recv(1024).decode()
+        c2state = c2.recv(1024).decode()
+        # receive paddle direction, ball location, and current score from clients
+    c1.close()
+    c2.close()
 
+def main(): 
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen(MAX_CONNS)
+  
+    while True: 
+        # attempt to pair two incoming clients 
+        clientSocket1, _ = server.accept()
+        print("Player one has connected.")
+        clientSocket2, _ = server.accept()
+        print("Player two has connected.")
+        
+        gameThread = threading.Thread(target=handleGame, args=(clientSocket1, clientSocket2))
+        gameThread.start()
 
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
